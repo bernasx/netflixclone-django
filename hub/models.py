ad6a918ab85e2f49059d4ec0e5d4271244b31d41
Bernas <bernasfixolas@hotmail.com>
2022-05-21 19:46:05 +0100

    Added a (possible) better way to represent things in the admin panel

--- hub/models.py
+++ hub/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     fullname = models.CharField(max_length=200, default='fullname')
-    pass
+    isProducer = models.BooleanField(default=False)
