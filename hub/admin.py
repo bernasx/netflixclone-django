39ce899aa35d12b21dd01917fc8d4465564e79a1
Bernas <bernasfixolas@hotmail.com>
2022-05-22 12:29:23 +0100

    Added remaining models to database schema and applied migrations

--- hub/admin.py
+++ hub/admin.py
@@ -2,10 +2,11 @@ from django import forms
 from django.contrib.auth.forms import UserChangeForm,UserCreationForm
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, Movie
+from .models import *
 
 
 # User
+# TODO - Add avatar and banner display to the admin panels for Users
 class MyUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
@@ -25,24 +26,36 @@ class UserAdmin(BaseUserAdmin):
             ('Custom', {'fields': ('fullname',)}),
     )
 
-# Movie
-class MovieChangeForm(forms.ModelForm):
+# Video
+# TODO - Add tags,video, thumbnail and banner display to the admin panels for Videos 
+class VideoChangeForm(forms.ModelForm):
     duration = forms.IntegerField(help_text='In seconds')
     quality = forms.CharField(help_text='[width]x[height]')
     class Meta:
-        model = Movie
+        model = Video
         fields = '__all__'
 
-class MovieAdmin(admin.ModelAdmin):
-    form = MovieChangeForm
+class VideoAdmin(admin.ModelAdmin):
+    form = VideoChangeForm
     fieldsets = [
         (None,{'fields': ('title', 'producer','quality','duration','description')}),
         ('Date information', {'fields': ('publish_date','lastUpdated',)}),
         ('Privacy', {'fields': ('isPublic','isUnlisted',)}),
     ]
     
-
+# View
+class ViewAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields': ('viewer','video','date')}),
+    ]
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
-admin.site.register(Movie, MovieAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(View, ViewAdmin)
+admin.site.register(Follower,admin.ModelAdmin)
+admin.site.register(Avatar,admin.ModelAdmin)
+admin.site.register(Banner,admin.ModelAdmin)
+admin.site.register(VideoBanner,admin.ModelAdmin)
+admin.site.register(Thumbnail,admin.ModelAdmin)
+admin.site.register(Storyboard,admin.ModelAdmin)
--- hub/models.py
+++ hub/models.py
@@ -1,11 +1,12 @@
+from datetime import datetime
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
-
+from django.utils import timezone
 class User(AbstractUser):
     fullname = models.CharField(max_length=200, default='fullname')
 
-class Movie(models.Model):
+class Video(models.Model):
     title = models.CharField(max_length=200, default='title')
     producer = models.ForeignKey(User, on_delete=models.CASCADE)
     publish_date = models.DateTimeField()
@@ -16,4 +17,42 @@ class Movie(models.Model):
     isPublic = models.BooleanField()
     isUnlisted = models.BooleanField()
     quality = models.CharField(max_length=200, default='1920x1080')
+    video = models.FileField(upload_to='uploads/videos/', default=None)
 
+class Tag(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=200, default='tag')
+
+class View(models.Model):
+    viewer = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+
+class Follower(models.Model):
+    producer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='producer')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+
+class Avatar(models.Model):
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     image = models.ImageField(upload_to='uploads/avatars/')
+     lastUpdated = models.DateTimeField(default=timezone.now)
+
+class Banner(models.Model):
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     image = models.ImageField(upload_to='uploads/user_banners/')
+     lastUpdated = models.DateTimeField(default=timezone.now)
+
+class VideoBanner(models.Model):
+     video = models.ForeignKey(Video, on_delete=models.CASCADE)
+     image = models.ImageField(upload_to='uploads/video_banners/')
+     lastUpdated = models.DateTimeField(default=timezone.now)
+
+class Thumbnail(models.Model):
+     video = models.ForeignKey(Video, on_delete=models.CASCADE)
+     image = models.ImageField(upload_to='uploads/video_thumbnails/')
+     lastUpdated = models.DateTimeField(default=timezone.now)
+
+class Storyboard(models.Model):
+     video = models.ForeignKey(Video, on_delete=models.CASCADE)
+     image = models.ImageField(upload_to='uploads/video_storyboards/')
+     lastUpdated = models.DateTimeField(default=timezone.now)
